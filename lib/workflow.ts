b5e67479cb3d2de32d2bd340e65a8d4f4a382de6
GuanHong Jiang <guanhongjiang@login.khoury.northeastern.edu>
2025-01-27 17:06:12 -0800

    implement qstash workflow for user onboarding

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,32 @@
 import config from "./config";
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QstashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken
 });
+
+const qstashClient = new QstashClient({ token: config.env.upstash.qstashToken });
+
+export const sendEmail = async ({
+  email, subject, message
+}: {
+  email: string,
+  subject: string,
+  message: string
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resend.resendToken }),
+    },
+    body: {
+      from: "<hello.guanhong.pro>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+
+}
